c35461746551a6ebdfbe52fcb516fb427bed7d93
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-04-30 14:59:24 -0500

    configure project

---
+++ greeting.js
@@ -0,0 +1,5 @@
+export const hello = async (event, context) => {
+    return {
+        "statusCode": 200, "body": JSON.stringify({message: "Hello world!"})
+    }
+}
--- handler.js
+++
@@ -1,8 +0,0 @@
-
-export const hello = async (event, context) => {
-    const queryParameters = event.queryStringParameters;
-    return {
-        "statusCode": 200,
-        "body": JSON.stringify({message: "xd", queryParameters})
-    }
-}
