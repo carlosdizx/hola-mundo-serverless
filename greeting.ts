fe3b144c3563ad81949f53fba779c09a4b9508ca
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-04-30 18:15:05 -0500

    add types of parameters and function

--- greeting.ts
+++ greeting.ts
@@ -1,5 +1,6 @@
-export const hello = async (event, context) => {
+import { Handler, APIGatewayProxyEvent, Context } from 'aws-lambda';
+export const hello: Handler = async (event: APIGatewayProxyEvent, context: Context) => {
     return {
-        "statusCode": 200, "body": JSON.stringify({message: "Hello world!"})
+        "statusCode": 200, "body": JSON.stringify({message: "Hello world xdxd"})
     }
 }
